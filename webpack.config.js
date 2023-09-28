ed4131fb29f27fc4c7ab0047bf822e9c9499caf0
dmitrygvl <dmitrygv1994@yandex.ru>
2023-09-28 18:52:17 +0300

    Add mini-css-extract-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   // context: "src",
@@ -17,7 +18,7 @@ module.exports = {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
       {
         test: /\.(png|svg|jpg|gif)$/i,
@@ -34,5 +35,8 @@ module.exports = {
       template: "src/index.html",
     }),
     new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "[name].[hash].css",
+    }),
   ],
 };
